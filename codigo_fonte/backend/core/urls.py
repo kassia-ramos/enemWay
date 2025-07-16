4733ddc04921068c9368b579fbef3bdcafd655d3
Kássia Ramos <Kassiaramos03@gmail.com>
2025-07-16 14:17:17 -0300

    Adiciona Dockerfiles, docker-compose e atualiza os arquivos de configuração

--- codigo_fonte/backend/core/urls.py
+++ codigo_fonte/backend/core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def home(request):
+    return HttpResponse("EnemWay backend está rodando!")
+
 urlpatterns = [
+    path('', home),  # raiz
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
